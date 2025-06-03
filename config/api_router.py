7983fd53aaddb4c9b69b3b7aa4ba56ef48e6f285
Abdi Berhe <abdiberhe@gmail.com>
2025-06-03 03:02:29 +0300

    Fix URL routing for events API and comment out auth token path

--- config/api_router.py
+++ config/api_router.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", include("event_scheduler.users.api.urls")),
     # Your stuff: custom urls includes go here
-    path("events/", include("event_scheduler.events.api.urls")),
+    path("", include("event_scheduler.events.api.urls")),
 ]
